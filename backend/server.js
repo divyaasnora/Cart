0978f9ed7e03559a41a59fda468976e6052791da
divyaasnora <divyaasnora@gmail.com>
2026-05-27 12:31:41 +0530

    Added GitHub Actions CI

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const axios = require("axios");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 require("dotenv").config();
 
